3e929bc67ea2508c385d7427b28218d889aca340
TanviKumar <tanvikumar777@gmail.com>
2017-11-30 15:00:45 +0530

    Add code for predicting for test set

--- multi_clf.py
+++ multi_clf.py
@@ -114,3 +114,23 @@ for e in range(epochs):
     test_accuracy = evaluate_accuracy(test_data, net)
     train_accuracy = evaluate_accuracy(train_data, net)
     print("Epoch %s. Loss: %s, Train_acc %s, Test_acc %s" % (e, cumulative_loss/num_examples, train_accuracy, test_accuracy))
+
+# The predictor. Returns prediction when we use our net.
+def model_predict(net,data):
+    output = net(data)
+    return nd.argmax(output, axis=1)
+
+# Sampling 10 random data points from the test set
+sample_data = mx.gluon.data.DataLoader(mnist_test, 10, shuffle=True)
+for i, (data, label) in enumerate(sample_data):
+    data = data.as_in_context(model_ctx)
+    print(data.shape)
+    im = nd.transpose(data,(1,0,2,3))
+    im = nd.reshape(im,(28,10*28,1))
+    imtiles = nd.tile(im, (1,1,3))
+    # Seeing the predictions after the training is done
+    plt.imshow(imtiles.asnumpy())
+    plt.show()
+    pred=model_predict(net,data.reshape((-1,784)))
+    print('model predictions are:', pred)
+    break
