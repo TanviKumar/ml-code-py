78a06d9d76d4c7031747eee6f850e39c18f147e5
TanviKumar <tanvikumar777@gmail.com>
2017-12-19 14:30:09 +0530

    cnn_scratch Init Parameters

--- cnn_scratch.py
+++ cnn_scratch.py
@@ -17,3 +17,28 @@ train_data = gluon.data.DataLoader(gluon.data.vision.MNIST(train=True, transform
                                       batch_size, shuffle=True)
 test_data = gluon.data.DataLoader(gluon.data.vision.MNIST(train=False, transform=transform),
                                      batch_size, shuffle=False)
+
+# Init Parameters
+weight_scale = .01
+num_fc = 128
+num_filter_conv_layer1 = 20
+num_filter_conv_layer2 = 20
+
+W1 = nd.random_normal(shape=(num_filter_conv_layer1
+	, 1, 3,3), scale=weight_scale, ctx=ctx)
+b1 = nd.random_normal(shape=num_filter_conv_layer1, scale=weight_scale, ctx=ctx)
+
+W2 = nd.random_normal(shape=(num_filter_conv_layer2, num_filter_conv_layer1, 5, 5), scale=weight_scale, ctx=ctx)
+b2 = nd.random_normal(shape=num_filter_conv_layer2, scale=weight_scale, ctx=ctx)
+
+W3 = nd.random_normal(shape=(320, num_fc), scale=weight_scale, ctx=ctx)
+b3 = nd.random_normal(shape=num_fc, scale=weight_scale, ctx=ctx)
+
+W4 = nd.random_normal(shape=(num_fc, num_outputs), scale=weight_scale, ctx=ctx)
+b4 = nd.random_normal(shape=num_outputs, scale=weight_scale, ctx=ctx)
+
+params = [W1, b1, W2, b2, W3, b3, W4, b4]
+
+# Attaching gradient to parameters
+for param in params:
+    param.attach_grad()                                     
