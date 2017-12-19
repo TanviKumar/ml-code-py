8724cdbb80fcab2f9e7c2e1a3c2ba604b0994ec6
TanviKumar <tanvikumar777@gmail.com>
2017-12-19 14:52:35 +0530

    cnn_gluon Define CNN

--- cnn_gluon.py
+++ cnn_gluon.py
@@ -17,3 +17,17 @@ train_data = gluon.data.DataLoader(gluon.data.vision.MNIST(train=True, transform
                                       batch_size, shuffle=True)
 test_data = gluon.data.DataLoader(gluon.data.vision.MNIST(train=False, transform=transform),
                                      batch_size, shuffle=False)
+
+num_fc = 512
+net = gluon.nn.Sequential()
+
+# Our neural net
+with net.name_scope():
+    net.add(gluon.nn.Conv2D(channels=20, kernel_size=5, activation='relu'))
+    net.add(gluon.nn.MaxPool2D(pool_size=2, strides=2))
+    net.add(gluon.nn.Conv2D(channels=50, kernel_size=5, activation='relu'))
+    net.add(gluon.nn.MaxPool2D(pool_size=2, strides=2))
+    # The Flatten layer collapses all axis, except the first one, into one axis.
+    net.add(gluon.nn.Flatten())
+    net.add(gluon.nn.Dense(num_fc, activation="relu"))
+    net.add(gluon.nn.Dense(num_outputs))
