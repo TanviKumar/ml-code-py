bdbce1dd3de1028bd96eae10b6f1386d74ee747d
TanviKumar <tanvikumar777@gmail.com>
2017-12-19 14:56:03 +0530

    cnn_gluon Add fn to find accuracy

--- cnn_gluon.py
+++ cnn_gluon.py
@@ -40,3 +40,14 @@ softmax_cross_entropy = gluon.loss.SoftmaxCrossEntropyLoss()
 
 # SGD for parameters
 trainer = gluon.Trainer(net.collect_params(), 'sgd', {'learning_rate': .1})
+
+# Function for finding accuracy
+def evaluate_accuracy(data_iterator, net):
+    acc = mx.metric.Accuracy()
+    for i, (data, label) in enumerate(data_iterator):
+        data = data.as_in_context(ctx)
+        label = label.as_in_context(ctx)
+        output = net(data)
+        predictions = nd.argmax(output, axis=1)
+        acc.update(preds=predictions, labels=label)
+    return acc.get()[1]
