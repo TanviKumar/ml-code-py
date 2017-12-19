0119674c84c5c72e1bb6c68b6b00313ef85762d5
TanviKumar <tanvikumar777@gmail.com>
2017-12-19 14:56:56 +0530

    cnn_gluon Add training loop

--- cnn_gluon.py
+++ cnn_gluon.py
@@ -51,3 +51,26 @@ def evaluate_accuracy(data_iterator, net):
         predictions = nd.argmax(output, axis=1)
         acc.update(preds=predictions, labels=label)
     return acc.get()[1]
+
+epochs = 1
+smoothing_constant = .01
+
+# Training loop
+for e in range(epochs):
+    for i, (data, label) in enumerate(train_data):
+        data = data.as_in_context(ctx)
+        label = label.as_in_context(ctx)
+        with autograd.record():
+            output = net(data)
+            loss = softmax_cross_entropy(output, label)
+        loss.backward()
+        trainer.step(data.shape[0])
+
+        #  Keep a moving average of the losses
+        curr_loss = nd.mean(loss).asscalar()
+        moving_loss = (curr_loss if ((i == 0) and (e == 0))
+                       else (1 - smoothing_constant) * moving_loss + smoothing_constant * curr_loss)
+
+    test_accuracy = evaluate_accuracy(test_data, net)
+    train_accuracy = evaluate_accuracy(train_data, net)
+    print("Epoch %s. Loss: %s, Train_acc %s, Test_acc %s" % (e, moving_loss, train_accuracy, test_accuracy))
